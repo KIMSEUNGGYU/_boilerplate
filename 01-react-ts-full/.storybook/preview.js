a2ae725761ee29d410dc71961bd108aee5c5ea08
kimseunggyu <dev.seunggyu@gmail.com>
2022-11-09 05:02:55 +0900

    cra-ts, storybook, msw

---
+++ 01-react-ts-full/.storybook/preview.js
@@ -0,0 +1,20 @@
+import '../src/index.css';
+
+import { withRouter } from 'storybook-addon-react-router-v6';
+export const decorators = [withRouter];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  options: {
+    storySort: {
+      order: ['Components', 'Page', 'Example'],
+    },
+  },
+  decorators,
+};
